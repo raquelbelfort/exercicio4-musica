3eb5bb7119c250d6c6e8e7515ddcbfee042ced48
Raquel Belfort <raquelzinha.belfort@gmail.com>
2020-02-28 21:54:07 -0300

    Adição da resolução.

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace exercicio4_musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.ReadKey();
+            Console.WriteLine("Dustin Tebbutt - Silk");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("You're one breath from my body");
+            Console.ReadKey();
+            Console.WriteLine("A satellite with stormy eyes");
+            Console.ReadKey();
+            Console.WriteLine("A heart that feels like running");
+            Console.ReadKey();
+            Console.WriteLine("But it's all right");
+            Console.ReadKey();
+            Console.WriteLine("The way that I see you there");
+            Console.ReadKey();
+            Console.WriteLine("While my chest comes undone");
+            Console.ReadKey();
+            Console.WriteLine("Like water runs and crests");
+            Console.ReadKey();
+            Console.WriteLine("No cease in this beating drum");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Tailor don't leave here yet");
+            Console.ReadKey();
+            Console.WriteLine("The silk we're seeking");
+            Console.ReadKey();
+            Console.WriteLine("Starts as thread");
+            Console.ReadKey();
+            Console.WriteLine("Tailor go easy");
+            Console.ReadKey();
+            Console.WriteLine("I know you don't see it but");
+            Console.ReadKey();
+            Console.WriteLine("It starts as thread");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("I see the dark clouds bloom");
+            Console.ReadKey();
+            Console.WriteLine("But I don't need a saviour");
+            Console.ReadKey();
+            Console.WriteLine("So come in I'll take you");
+            Console.ReadKey();
+            Console.WriteLine("Down here I'll lay you");
+            Console.ReadKey();
+            Console.WriteLine("Only warm and only true");
+            Console.ReadKey();
+            Console.WriteLine("I hear those mountains call");
+            Console.ReadKey();
+            Console.WriteLine("For something more but there's nothing more");
+            Console.ReadKey();
+            Console.WriteLine("And your shape as winter falls.");
+
+
+
+
+
+
+
+        }
+    }
+}
